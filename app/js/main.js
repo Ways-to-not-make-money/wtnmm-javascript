32cc8d993c96ca9baf8d7295b2fa1b9d9c2c9911
Derp McDerpson <DerpfacePython@users.noreply.github.com>
2016-08-12 20:51:07 +1000

    Added new function, "multiplier"

--- app/js/main.js
+++ app/js/main.js
@@ -1,85 +1,159 @@
-var amounts, buildingFunc, buildings, buildnames, clickUpg, clickcosts, clickeffects, clicknames, clickupgs, costs, hoverFuncBuild, hoverFuncUpg, hoverRemove, idleInc, idlecheck, inc, incamount, money, mps, mpsadds, toggleDisplay, totalclicks, upgcosts, upgeffects, upgnames, upgradeFunc, upgs;
+var amounts, buildingFunc, buildings, buildnames, clickUpg, clickcosts, clickeffects, clicklevel, clicknames, clickupgs, clickxp, convertCosts, costs, displayUpgs, idleInc, idlecheck, idlelevel, idlexp, inc, incamount, money, moneynameacr, mps, mpsadds, multiplier, multiplierChange, origcosts, round, toggleDisplay, totalclicks, totalearned, totalspent, upgcosts, upgeffects, upgnames, upgradeFunc, upgs,
+  indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
 incamount = 1.0;
 
 money = 0.0;
 
+moneynameacr = ["", "million", "billion", "trillion", "quadrillion", "quintillion"];
+
 mps = 0.0;
 
-amounts = [0, 0, 0, 0];
+multiplier = 1;
 
-costs = [8.0, 120.0, 1337.0, 20160.0];
+amounts = [0, 0, 0, 0, 0];
 
-mpsadds = [0.1, 0.4, 3.0, 7.5];
+costs = [8.0, 120.0, 1337.0, 20160.0, 123456.0];
 
-buildings = ["ac", "mp", "cc", "sc"];
+origcosts = [8.0, 120.0, 1337.0, 20160.0, 123456.0];
 
-buildnames = ["Auto-Clicker", "Money Printer", "Counterfeit Company", "Sharemarket Crash"];
+mpsadds = [0.1, 0.4, 3.0, 7.5, 32.1];
 
-upgs = [[0, 0], [0], [0], [0]];
+buildings = ["ac", "mp", "cc", "sc", "bh"];
 
-upgcosts = [[1000, 42000], [5000, 150000], [16900], [150000]];
+buildnames = ["Auto-Clicker", "Money Printer", "Counterfeit Company", "Sharemarket Crash", "Bank Heist"];
 
-upgeffects = [[1.5, 2.5], [2.0, 3.0], [2.0], [3.0]];
+upgs = [[0, 0], [0, 0], [0], [0], [0]];
 
-upgnames = [["Clicking Factories", "Iron-Clad Mice"], ["Skilled Fake Money Making", "Efficient Ink Cartridges"], ["Printing Templates"], ["Fire on Wall Street"]];
+upgcosts = [[100, 4200], [500, 15000], [1690, 40000], [50000], [500000]];
 
-clickupgs = [0];
+upgeffects = [[1.5, 2.5], [2.0, 3.0], [2.0, 4.0], [3.0], [3.5]];
 
-clickcosts = [2100];
+upgnames = [["Clicking Factories", "Iron-Clad Mice"], ["Printing Templates", "Efficient Ink Cartridges"], ["Skilled Fake Money Making", "Patented Products"], ["Fire on Wall Street"], ["Lockpicks"]];
 
-clickeffects = [3];
+clickupgs = [0, 0];
 
-clicknames = ["Flexible Fingers"];
+clickcosts = [210, 70000];
+
+clickeffects = [3, 10];
+
+clicknames = ["Flexible Fingers", "More Clicks!"];
 
 idlecheck = 0;
 
 totalclicks = 0;
 
+totalearned = 0;
+
+totalspent = [0, 0, 0, 0, 0];
+
+clicklevel = 1;
+
+clickxp = [0.0, 100.0];
+
+idlelevel = 1;
+
+idlexp = [0.0, 300.0];
+
+round = function(n, sigfigs) {
+  return parseFloat(n.toFixed(sigfigs));
+};
+
+convertCosts = function(n) {
+  var a, check, i, ref;
+  check = 0;
+  for (a = i = 1, ref = moneynameacr.length - 1; 1 <= ref ? i <= ref : i >= ref; a = 1 <= ref ? ++i : --i) {
+    if (n >= Math.pow(10, 6 + ((a - 1) * 3))) {
+      check += 1;
+    }
+  }
+  if (check === 0) {
+    return n.toFixed(1);
+  } else {
+    return (n / Math.pow(10, 6 + ((check - 1) * 3))).toFixed(1) + " " + moneynameacr[check];
+  }
+};
+
 inc = function() {
-  money = parseFloat((money + incamount).toFixed(3));
+  money = round(money + incamount, 3);
+  totalearned = round(totalearned + incamount, 3);
   totalclicks += 1;
-  $("#money").html("Balance: $" + money.toFixed(1));
+  clickxp[0] += 1;
+  if (clickxp[0] >= clickxp[1]) {
+    clicklevel += 1;
+    incamount = round(incamount * 1.5, 3);
+    clickxp[0] = round(clickxp[0] - clickxp[1], 3);
+    clickxp[1] = round(clickxp[1] * 1.1, 3);
+    $("#clicklevel").html("Clicking: Level " + clicklevel);
+    $("#clickearns").html("Click earns: $" + incamount);
+  }
+  $("#clickbar").width(round(clickxp[0] / clickxp[1] * 100, 1) + "%");
+  $("#clickxpamount").html(round(clickxp[0] / clickxp[1] * 100, 1) + "%");
+  $("#money").html("Balance: $" + convertCosts(money));
+  $("#earned").html("Total Money Earned: $" + convertCosts(totalearned));
   return $("#clicks").html("Total Clicks: " + totalclicks);
 };
 
 toggleDisplay = function(id) {
   if (id === "build") {
     $("#buildings").toggle();
-    if ($("#buildings").css("display") !== "none") {
-      $("#upgrades").hide();
-      return $("#stats").hide();
-    }
-  } else if (id === "upg") {
-    $("#upgrades").toggle();
-    if ($("#upgrades").css("display") !== "none") {
-      $("#buildings").hide();
-      return $("#stats").hide();
+    if ($("#buildings").css("display" !== "none")) {
+      $("#stats").hide();
+      $("#statsbutton").css("background-color", "grey");
+      return $("#buildbutton").css("background-color", "#5555cc");
+    } else {
+      return $("#buildbutton").css("background-color", "grey");
     }
   } else {
     $("#stats").toggle();
-    if ($("#stats").css("display") !== "none") {
+    if ($("#stats").css("display" !== "none")) {
       $("#buildings").hide();
-      return $("#upgrades").hide();
+      $("#buildbutton").css("background-color", "grey");
+      return $("#statsbutton").css("background-color", "#5555cc");
+    } else {
+      return $("#statsbutton").css("background-color", "grey");
     }
   }
 };
 
 idleInc = function() {
-  money = (money + (mps / 100)).toFixed(3);
-  $("#money").html("Balance: $" + parseFloat(money).toFixed(1));
-  money = parseFloat(money);
+  var b, i, ref;
+  money = round(money + (mps / 100), 3);
+  totalearned = round(totalearned + (mps / 100), 3);
+  idlexp[0] = round(idlexp[0] + (mps / 100), 3);
+  if (idlexp[0] >= idlexp[1]) {
+    idlelevel += 1;
+    for (b = i = 0, ref = mpsadds.length; 0 <= ref ? i <= ref : i >= ref; b = 0 <= ref ? ++i : --i) {
+      mpsadds[b] = round(mpsadds[b] * 1.1, 3);
+    }
+    mps = round(mps * 1.1, 3);
+    idlexp[0] = round(idlexp[0] - idlexp[1], 3);
+    idlexp[1] = round(idlexp[1] * 1.5, 3);
+    $("#idlelevel").html("Idling: Level " + idlelevel);
+  }
+  $("#idlebar").width(round(idlexp[0] / idlexp[1] * 100, 1) + "%");
+  $("#idlexpamount").html(round(idlexp[0] / idlexp[1] * 100, 1) + "%");
+  $("#money").html("Balance: $" + convertCosts(money));
+  $("#earned").html("Total Money Earned: $" + convertCosts(totalearned));
   window.setTimeout(idleInc, 10);
 };
 
 buildingFunc = function(n) {
+  var c, i, ref;
   if (money >= costs[n]) {
     money -= costs[n];
-    costs[n] = parseFloat((costs[n] * 1.15).toFixed(3));
-    amounts[n] += 1;
-    $("#" + buildings[n] + "build").html(buildnames[n] + " ($" + costs[n].toFixed(1) + ")");
-    mps = parseFloat((mps + mpsadds[n]).toFixed(1));
-    $("#mps").html("$" + mps + "/second");
+    totalspent[n] = round(totalspent[n] + costs[n], 3);
+    costs[n] = round(costs[n] * (Math.pow(1.15, multiplier)), 3);
+    if (multiplier !== 1) {
+      for (c = i = 1, ref = multiplier; 1 <= ref ? i <= ref : i >= ref; c = 1 <= ref ? ++i : --i) {
+        costs[n] = round(costs[n] + Math.pow(1.15, origcosts[n] + c), 3);
+      }
+    }
+    amounts[n] += multiplier;
+    $("#" + buildings[n] + "build").html(buildnames[n] + "<br>($" + convertCosts(costs[n]) + ")");
+    mps = round(mps + mpsadds[n], 1);
+    $("#mps").html("$" + convertCosts(mps) + "/second");
+    $("#" + buildings[n] + "stats").html("Amount: " + amounts[n] + "<br>$" + convertCosts(mpsadds[n] * amounts[n]) + "/second<br>Total Spent: $" + convertCosts(totalspent[n]));
     if (idlecheck === 0) {
       idlecheck += 1;
       idleInc();
@@ -93,9 +167,11 @@ upgradeFunc = function(n, step) {
   if (money >= upgcosts[n][step] && amounts[n] >= 1) {
     money -= upgcosts[n][step];
     upgs[n][step] += 1;
-    mpsadds[n] *= upgeffects[n][step];
+    $("#" + buildings[n] + "upg" + (step + 1)).css("margin-bottom", "0");
     $("#" + buildings[n] + "upg" + (step + 1)).hide();
-    mpsadds[n] = parseFloat((mpsadds[n] * upgeffects[n][step]).toFixed(3));
+    mps += amounts[n] * (mpsadds[n] * (upgeffects[n][step] - 1));
+    mpsadds[n] = round(mpsadds[n] * upgeffects[n][step], 3);
+    $("#mps").html("$" + convertCosts(mps) + "/second");
   } else {
     window.alert("You do not have enough money.");
   }
@@ -103,46 +179,57 @@ upgradeFunc = function(n, step) {
 
 clickUpg = function(n) {
   if (money >= clickcosts[n]) {
-    money -= clickcost[n];
+    money -= clickcosts[n];
     clickupgs[n] += 1;
     incamount *= clickeffects[n];
+    $("#clickupg" + (n + 1)).css("margin-bottom", "0");
     return $("#clickupg" + (n + 1)).hide();
   } else {
     return window.alert("You do not have enough money.");
   }
 };
 
-hoverFuncBuild = function(n) {
-  var boughtupgs, upgcheck;
-  boughtupgs = [];
-  upgcheck = 0;
-  while (upgcheck < upgs[n].length) {
-    if (upgs[n][upgcheck] === 1) {
-      boughtupgs.push(upgnames[n][upgcheck] + " (*" + upgeffects[n][upgcheck] + "MPS)");
-    }
-    upgcheck += 1;
-  }
-  if (boughtupgs.length === 0) {
-    boughtupgs.push("None yet!");
-  }
-  boughtupgs = boughtupgs.join("<br>");
-  return $("#hoverinfo").html(buildnames[n] + "<br>Amount: " + amounts[n] + "<br>Total MPS: " + (amounts[n] * mpsadds[n]).toFixed(1) + "<br>Upgrades unlocked:<br>" + boughtupgs);
-};
-
-hoverFuncUpg = function(n, step) {
-  var hovertemp1, hovertemp2, hovertemp3;
+displayUpgs = function(n) {
+  var upgtemp;
   if (n === -1) {
-    hovertemp1 = clicknames[step];
-    hovertemp2 = "Main Button ";
-    hovertemp3 = clickeffects[step];
+    upgtemp = "click";
   } else {
-    hovertemp1 = upgnames[n][step];
-    hovertemp2 = buildnames[n];
-    hovertemp3 = upgeffects[n][step] + "MPS";
+    upgtemp = buildings[n];
   }
-  return $("#hoverinfo").html(hovertemp1 + "<br>Effects:<br>" + hovertemp2 + " * " + hovertemp3);
+  if ($("#" + upgtemp + "upglist").css("display" !== "none")) {
+    $("#" + upgtemp + "displayupgs").html("Open Upgrades");
+    $("#" + upgtemp + "displayupgs").css("background-color", "grey");
+  } else {
+    $("#" + upgtemp + "displayupgs").html("Close Upgrades");
+    $("#" + upgtemp + "displayupgs").css("background-color", "#5555cc");
+  }
+  return $("#" + upgtemp + "upglist").toggle();
 };
 
-hoverRemove = function() {
-  return $("#hoverinfo").html("Hover over something!");
+multiplierChange = function() {
+  var d, e, f, g, i, j, k, l, multipliercolours, multiplierlist, ref, ref1, ref2, ref3, results;
+  multiplierlist = [1, 10, 100];
+  multipliercolours = ["orange", "yellow", "chartreuse"];
+  for (d = i = 0, ref = costs.length - 1; 0 <= ref ? i <= ref : i >= ref; d = 0 <= ref ? ++i : --i) {
+    costs[d] = round(origcosts[d] * (Math.pow(1.15, amounts[d])), 3);
+  }
+  if (multiplier === 100) {
+    multiplier = 1;
+    $("#multiplier").html("x1");
+    $("#multiplier").css("background-color", "orange");
+  } else if (indexOf.call(multiplierlist.slice(0, 2), multiplier) >= 0) {
+    multiplier = multiplierlist[multiplierlist.indexOf(multiplier + 1)];
+    $("#multiplier").html("x" + multiplier);
+    $("#multiplier").css("background-color", multipliercolours[multiplierlist.indexOf(multiplier)]);
+    for (e = j = 1, ref1 = multiplier - 1; 1 <= ref1 ? j <= ref1 : j >= ref1; e = 1 <= ref1 ? ++j : --j) {
+      for (f = k = 0, ref2 = costs.length - 1; 0 <= ref2 ? k <= ref2 : k >= ref2; f = 0 <= ref2 ? ++k : --k) {
+        costs[f] = round(costs[f] + origcosts[f] * (Math.pow(1.15, amounts[f] + e)), 3);
+      }
+    }
+  }
+  results = [];
+  for (g = l = 0, ref3 = buildings.length - 1; 0 <= ref3 ? l <= ref3 : l >= ref3; g = 0 <= ref3 ? ++l : --l) {
+    results.push($("#" + buildings[g] + "build").html(buildnames[g] + "<br>($" + convertCosts(costs[g]) + ")"));
+  }
+  return results;
 };
